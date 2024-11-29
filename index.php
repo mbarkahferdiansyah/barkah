54315bc868a73e435c4055d06f81a88725f9e758
mbarkahferdiansyah <mbarkaferdiansyah@gmail.com>
2024-11-29 13:26:25 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<H1> Nama Anggota 1: M.Barkah Ferdiansyah <H/1>
+<H1> NPM: G1C024014 <H/1>
+<H1> NPM: G1C024014 <H/1>
+<H1> Nama Anggota 2: Muhammad Daffasyah <H/1>
+<H1> NPM: G1C024010 <H/1>
+
+<H1> Mata Kuliah: Coding <H/1>
+<H1> Kelas: B <H/1>
+
+<H1> Kata-kata Mutiara: Bermimpilah setinggi langit dan jatuhlah di antara bintang-bintang <H/1>
